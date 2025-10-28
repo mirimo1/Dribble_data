05aa9790d46dd463f89c75789fbf93f408886bc9
Pascal Mirimo <pascalmirimo57@gmail.com>
2025-10-28 12:56:33 -0700

    Add Player and Game models, serializers, views, and API routing

--- dribble_core/stats/models.py
+++ dribble_core/stats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    name = models.CharField(max_length=100)
+    team = models.CharField(max_length=50)
+    position = models.CharField(max_length=20)
+    points_per_game = models.FloatField()
+    rebounds_per_game = models.FloatField()
+    assists_per_game = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+class Game(models.Model):
+    date = models.DateField()
+    home_team = models.CharField(max_length=50)
+    away_team = models.CharField(max_length=50)
+    home_score = models.IntegerField()
+    away_score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.home_team} vs {self.away_team} on {self.date}"
--- dribble_core/stats/views.py
+++ dribble_core/stats/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Player, Game
+from .serializers import PlayerSerializer, GameSerializer
 
-# Create your views here.
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
